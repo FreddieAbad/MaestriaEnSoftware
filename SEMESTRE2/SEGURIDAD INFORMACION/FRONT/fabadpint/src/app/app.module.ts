fd0f6c44c5d1d0f2e081813f02bffb3df1212c9f
Freddy <freddy.abadl@ucuenca.edu.ec>
2024-02-08 01:01:30 -0500

    Update Frontend with fixes funcionality

--- SEMESTRE2/SEGURIDAD INFORMACION/FRONT/fabadpint/src/app/app.module.ts
+++ SEMESTRE2/SEGURIDAD INFORMACION/FRONT/fabadpint/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms'; 
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SignupComponent } from './signup/signup.component';
 import { HomeauthComponent } from './homeauth/homeauth.component';
 import { ConfigurepyscanComponent } from './configurepyscan/configurepyscan.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,10 @@ import { ConfigurepyscanComponent } from './configurepyscan/configurepyscan.comp
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [
     provideClientHydration()
--- SEMESTRE2/SEGURIDAD INFORMACION/FRONT/fabadpint/src/app/homeauth/homeauth.component.ts
+++ SEMESTRE2/SEGURIDAD INFORMACION/FRONT/fabadpint/src/app/homeauth/homeauth.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { API_URL_GSB_LISTAR_NAVEGACION } from '../config';
 import { API_URL_BUSCAR_AMENAZA } from '../config';
-import { API_URL_LISTAR_NAVEGACION } from '../config';
 import { API_URL_IPINFO } from '../config';
 import { API_URL_VT_CONSULTAR_ARCHIVOS_IP } from '../config';
 import { API_URL_VT_CONSULTAR_IP } from '../config';
+import { saveAs } from 'file-saver';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 interface LastAnalysisResult {
   engine_name: string;
@@ -25,70 +29,93 @@ export class HomeauthComponent {
 
   ipInputVT: string = '';
   ipInfoVT: any;
+  urlRegex: RegExp = /^(?:(?:https?|sdp|ftp(?:s)?|ssh):\/\/)?(?:www\.)?[^\s/$.?#].[^\s]*$/;
 
   ipv4Pattern: string = '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$';
+  fileToUpload: File | null = null;
 
-  constructor(private http: HttpClient) {}
-  ngOnInit() {
-    this.listadoNavegacion();
-  }
-  escanearAmenazas() {
-    const body = { url: this.url };
+  constructor(private http: HttpClient, private spinner: NgxSpinnerService, private toastr: ToastrService, private router: Router) { }
 
-    this.http.post(API_URL_BUSCAR_AMENAZA, body)
-      .subscribe(
-        (data: any) => {
-          this.response = data;
-          console.log('Respuesta del servidor:', data);
-        },
-        error => {
-          console.error('Error en la petición:', error);
-        }
-      );
-  }
 
+
+  //1 LISTO consulta de lista de amenazas
   listadoNavegacion() {
-    this.http.get<any>(API_URL_LISTAR_NAVEGACION).subscribe(
+    this.spinner.show();
+    this.http.get<any>(API_URL_GSB_LISTAR_NAVEGACION).subscribe(
       response => {
+        this.spinner.hide();
+        this.toastr.success('Consulta realizada correctamente.', 'Éxito.');
+
         this.threatLists = response.threatLists;
       },
       error => {
-        console.error('Error al obtener las listas de amenazas:', error);
+        this.spinner.hide();
+        this.toastr.info('Estamos teniendo problemas técnicos. Por favor, intenta nuevamente en unos minutos.', 'Oops.');
       }
     );
   }
-
-  verificarIP() {
-    if (!this.ipInput.match(this.ipv4Pattern)) {
-      alert('Dirección IP no válida');
+  //2 LISTO escanea amenazas con google safe browsing
+  escanearAmenazas() {
+    if (!this.urlRegex.test(this.url)) {
+      this.toastr.info('Debes ingresar valores tipo url.', 'Datos incompletos.');
       return;
     }
-    // 'http://127.0.0.1:5000/ipinfo/verificar_ip'
+    const body = { url: this.url };
+    this.spinner.show();
+    this.http.post(API_URL_BUSCAR_AMENAZA, body)
+      .subscribe(
+        (data: any) => {
+          this.spinner.hide();
+          this.toastr.success('Consulta realizada correctamente.', 'Éxito.');
+          this.response = data;
+        },
+        error => {
+          this.spinner.hide();
+          this.toastr.info('Estamos teniendo problemas técnicos. Por favor, intenta nuevamente en unos minutos.', 'Oops.');
+        }
+      );
+  }
+  //3 listo verifica IP con ipinfo
+  verificarIP() {
+    if (!this.ipInput.match(this.ipv4Pattern)) {
+      this.toastr.info('Dirección IPv4 no válida.', 'Datos incorrectos.');
+      return;
+    }
+    this.spinner.show();
     this.http.post<any>(API_URL_IPINFO, { ip: this.ipInput })
       .subscribe(
         response => {
+          this.spinner.hide();
+          this.toastr.success('Consulta realizada correctamente.', 'Éxito.');
           this.ipInfo = response;
         },
         error => {
-          console.error('Error al verificar la IP:', error);
+          this.spinner.hide();
+          this.toastr.info('Estamos teniendo problemas técnicos. Por favor, intenta nuevamente en unos minutos.', 'Oops.');
         }
       );
   }
 
-
+  getLastAnalysisResults(results: any): string[] {
+    return Object.keys(results);
+  }
+  //validar IPsv4
   verificarIP_VT() {
-    if (!this.ipInput.match(this.ipv4Pattern)) {
-      alert('Dirección IP no válida');
+    if (!this.ipInputVT.match(this.ipv4Pattern)) {
+      this.toastr.info('Dirección IPv4 no válida.', 'Datos incorrectos.');
       return;
     }
-    const body = { ip: this.ipInputVT }; 
-    // const url = 'http://127.0.0.1:5000/virus_total/consultar_ip'; 
-
+    const body = { ip: this.ipInputVT };
+    this.spinner.show();
     this.http.post(API_URL_VT_CONSULTAR_IP, body).subscribe(
       (response) => {
+        this.spinner.hide();
+        this.toastr.success('Consulta realizada correctamente.', 'Éxito.');
         this.ipInfoVT = response;
       },
       (error) => {
+        this.spinner.hide();
+        this.toastr.info('Estamos teniendo problemas técnicos. Por favor, intenta nuevamente en unos minutos.', 'Oops.');
         console.error('Error al consultar la IP:', error);
       }
     );
@@ -102,4 +129,62 @@ export class HomeauthComponent {
     const lastResult: LastAnalysisResult = Object.values(results).pop() as LastAnalysisResult;
     return `${lastResult.engine_name}: ${lastResult.result}`;
   }
+
+  //genera texto de prueba
+  generateTxtFile() {
+    const content = `209.99.40.219
+78.138.126.220
+78.138.126.223`;
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'archivo_de_prueba.txt';
+    a.click();
+    window.URL.revokeObjectURL(url);
+
+  }
+
+
+
+  //validacion de seleccion de archivo
+  onFileSelected(event: any) {
+    this.fileToUpload = event.target.files[0];
+    if (!this.fileToUpload || !this.fileToUpload.name.endsWith('.txt')) {
+      alert('Por favor, seleccione un archivo .txt válido.');
+      this.fileToUpload = null;
+    }
+  }
+  //procesar archivo cargado para consulta de IPs
+  processFile() {
+    if (!this.fileToUpload) {
+      this.toastr.info('Por favor, seleccione un archivo.', 'Oops.');
+      return;
+    }
+    const formData = new FormData();
+    formData.append('file', this.fileToUpload);
+    this.spinner.show();
+    this.http.post(API_URL_VT_CONSULTAR_ARCHIVOS_IP, formData, { responseType: 'text' })
+      .subscribe(
+        response => {
+          this.spinner.hide();
+          console.log('El archivo se ha enviado correctamente al servidor.' + response);
+          const blob = new Blob([response], { type: 'text/csv' });
+          saveAs(blob, 'resultados.csv');
+          this.toastr.success('Procesamiento realizado correctamente.', 'Éxito.');
+        },
+        error => {
+          this.spinner.hide();
+          this.toastr.info('Estamos teniendo problemas técnicos. Por favor, intenta nuevamente en unos minutos.', 'Oops.');
+
+        }
+      );
+  }
+  goToSignUpPage() {
+    this.router.navigateByUrl('/signup');
+  }
+  goToLoginPage() {
+    this.router.navigateByUrl('/');
+  }
+
 }
